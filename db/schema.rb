d3302772fb6256c0fccfaddfeaaefb0ba51d40d1
tom.kawat <tomozero99@gmail.com>
2018-12-01 14:04:29 +0000

    kadai Tasklist add new column 課題カラムの追加です

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_30_114035) do
+ActiveRecord::Schema.define(version: 2018_12_01_125107) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
